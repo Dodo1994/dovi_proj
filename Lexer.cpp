4326146aa5cacc0d1634a9af103e4dc8be76c2b6
DebuggerOR <35989750+DebuggerOR@users.noreply.github.com>
2018-12-12 15:13:47 +0200

    Update Lexer.cpp

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,36 +1,33 @@
 
 #include "Lexer.h"
 
-bool is_space(char c) {
+bool Lexer::is_space(char c) {
     return (c == ' ' || c == '\n' || c == '\t');
 }
 
 list <string> Lexer::string_to_list(string input) {
-    list<string> words;
-
     int i = 0;
     string word;
     bool isFirstSpace = true;
+    list<string> words;
 
+    // pass over input
     while (i < input.size()) {
         // pass over spaces
         while (is_space(input[i]) && i < input.size()) {
-
+            // if first space after word
             if(isFirstSpace) {
                 isFirstSpace = false;
-
                 // add word and init word
                 words.push_back(word);
                 word="";
             }
-
             ++i;
         }
         // add char to word
         word+=input[i];
-
-        isFirstSpace= true;
-
+        // init first space
+        isFirstSpace = true;
         ++i;
     }
 
