646d2e70b93977ad091c8640a944b48d4bbb2582
DebuggerOR <foglerori@gmail.com>
2018-12-14 10:32:37 +0200

    added all classes Eli mentioned

--- Interpreter.h
+++ Interpreter.h
@@ -6,8 +6,11 @@ using namespace std;
 
 #include <string>
 #include <list>
+#include <map>
+#include "Expression.h"
 
 class interpreter {
+    map<string, Expression> vars_map;
 
 public:
     list<string> lexer(string input);
